786e5e923648c10a07e45eac9d4eda3ce706cde0
Richard Petri <richard.petri@bmw.de>
2023-02-16 16:51:53 +0100

    docs: Clarify what statistics in the time_stats function are about

--- bikeshare.py
+++ bikeshare.py
@@ -113,7 +113,7 @@ def load_data(city, month, day):
 
 
 def time_stats(df):
-    """Displays statistics on the most frequent times of travel."""
+    """Displays statistics on the most frequent times of travel (Month, day of the week and starting hour)."""
 
     print('\nCalculating The Most Frequent Times of Travel...\n')
     start_time = time.time()
